fe943b18aa45571258c34352096afdbea928af87
vednoc <vednoc@protonmail.com>
2021-02-01 20:24:14 +0100

    feat: show logged-in state in navbar on Home page

--- handlers/core/home.go
+++ handlers/core/home.go
@@ -1,11 +1,27 @@
 package core
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+
+	"userstyles.world/handlers/sessions"
+)
+
+var (
+	store = sessions.GetStore()
 )
 
 func Home(c *fiber.Ctx) error {
+	s, err := store.Get(c)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(s, err, s.Get("name"))
+
 	return c.Render("index", fiber.Map{
+		"Name":  s.Get("name"),
 		"Title": "Home",
 	})
 }
