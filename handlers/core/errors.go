48cdf01aa70f08d2031c5db9cb95c281e728d081
vednoc <vednoc@protonmail.com>
2021-02-02 05:24:10 +0100

    feat: show logged-in state in navbar on 404 page

--- handlers/core/errors.go
+++ handlers/core/errors.go
@@ -1,11 +1,19 @@
 package core
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func NotFound(c *fiber.Ctx) error {
+	s, err := store.Get(c)
+	if err != nil {
+		log.Println(err)
+	}
+
 	return c.Render("404", fiber.Map{
+		"Name":  s.Get("name"),
 		"Title": "Page not found",
 	})
 }
