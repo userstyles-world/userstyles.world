44dc8b05c8e6edad1e43e8e46a0474e74a9899b1
vednoc <vednoc@protonmail.com>
2021-02-12 18:14:06 +0100

    feat: print Styles debug on user's Account page

--- handlers/user/account.go
+++ handlers/user/account.go
@@ -2,7 +2,10 @@ package user
 
 import (
 	"github.com/gofiber/fiber/v2"
+
+	"userstyles.world/database"
 	"userstyles.world/handlers/sessions"
+	"userstyles.world/models"
 )
 
 func Account(c *fiber.Ctx) error {
@@ -17,8 +20,16 @@ func Account(c *fiber.Ctx) error {
 		})
 	}
 
+	styles, err := models.GetStylesByUser(database.DB, s.Get("name").(string))
+	if err != nil {
+		return c.Render("err", fiber.Map{
+			"Name":  s.Get("name"),
+			"Title": "Server error",
+		})
+	}
 	return c.Render("account", fiber.Map{
-		"Name":  s.Get("name"),
-		"Title": "Account",
+		"Name":   s.Get("name"),
+		"Title":  "Account",
+		"Styles": styles,
 	})
 }
