c411cd025978cccf8829c94156de494e736c3599
Gusted <williamzijl7@hotmail.com>
2021-03-29 17:57:13 +0200

    feat: SMTP wrapper prototype

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"userstyles.world/database"
 	"userstyles.world/handlers"
 	"userstyles.world/updater"
@@ -8,6 +10,18 @@ import (
 )
 
 func main() {
+	err := utils.NewEmail().
+		SetDefaultFrom().
+		SetTo("HERE YOUR EMAIL").
+		SetSubject("Verify your email address").
+		SetPlainBody("Simple plain email").
+		SetHTMLBody("<h3>Simple HTML body</h3>").
+		SendEmail()
+
+	if err != nil {
+		log.Fatalf("Couldn't send email due to %s", err)
+	}
+
 	utils.InitializeValidator()
 	database.Initialize()
 	updater.Initialize()
--- utils/email.go
+++ utils/email.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
-	"fmt"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"regexp"
 	"strings"
 
 	"github.com/emersion/go-sasl"
@@ -9,13 +12,97 @@ import (
 	"userstyles.world/config"
 )
 
-var auth = sasl.NewPlainClient("", config.EMAIL_ADDRESS, config.EMAIL_PWD)
+var (
+	auth      = sasl.NewPlainClient("", config.EMAIL_ADDRESS, config.EMAIL_PWD)
+	emailLine = regexp.MustCompile(`(\\r)?\\n`)
+	CLRF      = "\r\n"
+)
 
-func SendEmail(to, subject, message string) error {
-	r := strings.NewReader(fmt.Sprintf("To: %s\r\n"+
-		"From: %s\r\n"+
-		"Subject: %s\r\n"+
-		"\r\n"+
-		"%s\r\n", to, config.EMAIL_ADDRESS, subject, message))
-	return smtp.SendMail("mail.userstyles.world:587", auth, config.EMAIL_ADDRESS, []string{to}, r)
+type EmailBuilder struct {
+	To        string
+	From      string
+	Subject   string
+	PlainBody string
+	HTMLBody  string
+	boundary  string
+}
+
+func NewEmail() *EmailBuilder {
+	return &EmailBuilder{}
+}
+
+func (eb *EmailBuilder) SetTo(to string) *EmailBuilder {
+	eb.To = to
+	return eb
+}
+
+func (eb *EmailBuilder) SetFrom(from string) *EmailBuilder {
+	eb.From = from
+	return eb
+}
+
+func (eb *EmailBuilder) SetDefaultFrom() *EmailBuilder {
+	eb.From = config.EMAIL_ADDRESS
+	return eb
+}
+
+func (eb *EmailBuilder) SetSubject(subject string) *EmailBuilder {
+	eb.Subject = subject
+	return eb
+}
+
+func (eb *EmailBuilder) SetPlainBody(message string) *EmailBuilder {
+	eb.PlainBody = message
+	return eb
+}
+
+func (eb *EmailBuilder) SetHTMLBody(htmlMessage string) *EmailBuilder {
+	eb.HTMLBody = htmlMessage
+	return eb
+}
+
+func randomBoundary() (string, error) {
+	buf := make([]byte, 30)
+	_, err := io.ReadFull(rand.Reader, buf)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(buf), nil
+}
+
+func (eb *EmailBuilder) parseMultiPart() string {
+	boundary := "--" + eb.boundary
+	output := boundary + "\n" +
+		"Content-Type: text/plain; charset=\"utf-8\"; format=\"fixed\"\n" +
+		"Content-Transfer-Encoding: quoted-printable\n" +
+		"\n" +
+		eb.PlainBody +
+		"\n\n" +
+		boundary + "\n" +
+		"Content-Type: text/html; charset=UTF-8\n" +
+		"Content-Transfer-Encoding: quoted-printable\n" +
+		"\n" +
+		"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
+		eb.HTMLBody
+	return output
+}
+
+func correctLineBreak(message string) string {
+	return string(emailLine.ReplaceAll([]byte(message), []byte(CLRF)))
+}
+
+func (eb *EmailBuilder) SendEmail() error {
+	boundary, err := randomBoundary()
+	if err != nil {
+		return err
+	}
+	eb.boundary = boundary
+	r := strings.NewReader(correctLineBreak("To: " + eb.To + "\n" +
+		"From:" + eb.From + "\n" +
+		"Subject:" + eb.Subject + "\n" +
+		"Content-Type: multipart/alternative; boundary=\"" + eb.boundary + "\"\n" +
+		"MIME-Version: 1.0\n" +
+		"\n" +
+		eb.parseMultiPart()))
+	return smtp.SendMail("mail.userstyles.world:587", auth, config.EMAIL_ADDRESS, []string{eb.To}, r)
 }
