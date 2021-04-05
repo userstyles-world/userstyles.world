d3b9b62c5a0298ba0800439d7989278c7a448204
vednoc <vednoc@protonmail.com>
2021-04-05 14:08:57 +0200

    fix: properly handle 404 errors on API route

--- handlers/core/errors.go
+++ handlers/core/errors.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	"userstyles.world/handlers/jwt"
 )
@@ -8,8 +10,15 @@ import (
 func NotFound(c *fiber.Ctx) error {
 	u, _ := jwt.User(c)
 
-	return c.Render("err", fiber.Map{
-		"User":  u,
-		"Title": "Page not found",
-	})
+	// Handle 404 errors on API route, otherwise render error template.
+	if strings.HasPrefix(c.OriginalURL(), "/api/") {
+		return c.JSON(fiber.Map{
+			"error": "bad endpoint",
+		})
+	} else {
+		return c.Render("err", fiber.Map{
+			"User":  u,
+			"Title": "Page not found",
+		})
+	}
 }
