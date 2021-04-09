5fa443154f3ed91b1432a72b5493086e6a255ab8
Gusted <williamzijl7@hotmail.com>
2021-04-09 15:06:46 +0200

    refactor: update in batches

--- updater/handler.go
+++ updater/handler.go
@@ -8,23 +8,42 @@ import (
 	"userstyles.world/models"
 )
 
-var UpdaterTick time.Duration
+var (
+	startupDelay time.Duration
+	updaterTick  time.Duration
+	batchSize    int
+)
 
 func Initialize() {
-	UpdaterTick = time.Minute * 30
+	startupDelay = time.Second * 15
+	updaterTick = time.Minute * 30
+	batchSize = 5
 	go Tick()
 }
 
 func Tick() {
+	time.Sleep(startupDelay)
 	for {
 		importedStyles, err := models.GetAllImportedStyles(database.DB)
 		if err == nil {
-			for i, len := 0, len(*importedStyles); i < len; i++ {
-				go UpdateBatch((*importedStyles)[i])
+			stylesLen := len(*importedStyles)
+			n, i := 0, 0
+		out:
+			for {
+				for n < batchSize {
+					if i >= stylesLen {
+						break out
+					}
+					go UpdateBatch((*importedStyles)[i])
+					i++
+					n++
+				}
+				time.Sleep(time.Millisecond * 50)
+				n = 0
 			}
 		} else {
 			log.Printf("Updater: Wannted to update imported styles, but caught error: %s", err)
 		}
-		time.Sleep(UpdaterTick)
+		time.Sleep(updaterTick)
 	}
 }
