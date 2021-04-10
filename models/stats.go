8e4e9389513784a626a8ad493ee137885592d8d4
vednoc <vednoc@protonmail.com>
2021-04-10 21:46:39 +0200

    feat: hash unique record for Style statistics

--- models/stats.go
+++ models/stats.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/hex"
 	"errors"
 	"log"
 	"strconv"
@@ -25,7 +28,17 @@ func AddStatsForStyle(db *gorm.DB, id, ip string) (Stats, error) {
 		return *s, errors.New("failed to convert StyleID to int")
 	}
 
-	s.Hash = ip + " " + id
+	// TODO: Refactor as GenerateHashedRecord; we have a circular dependency now.
+	record := ip + " " + id
+	secret := "secretkey" // TODO: Add another key for this.
+
+	// Generate unique hash.
+	h := hmac.New(sha512.New, []byte(secret))
+	h.Write([]byte(record))
+	sha := hex.EncodeToString(h.Sum(nil))
+
+	// Set values.
+	s.Hash = sha
 	s.StyleID = styleID
 
 	err = db.
