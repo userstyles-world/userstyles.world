b9641a1485649101c650532381fa5bb0ce39ecd2
vednoc <vednoc@protonmail.com>
2021-04-10 21:18:43 +0200

    feat: add a way to save unique info for statistics

--- models/stats.go
+++ models/stats.go
@@ -4,13 +4,15 @@ import (
 	"errors"
 	"log"
 	"strconv"
+	"time"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type Stats struct {
 	gorm.Model
-	IP      string
+	Hash    string `gorm:"unique"`
 	StyleID int
 	Style   Style
 }
@@ -23,12 +25,18 @@ func AddStatsForStyle(db *gorm.DB, id, ip string) (Stats, error) {
 		return *s, errors.New("failed to convert StyleID to int")
 	}
 
-	s.IP = ip
+	s.Hash = ip + " " + id
 	s.StyleID = styleID
 
 	err = db.
 		Debug().
 		Model(s).
+		Clauses(clause.OnConflict{
+			Columns: []clause.Column{{Name: "hash"}},
+			DoUpdates: clause.Assignments(map[string]interface{}{
+				"updated_at": time.Now(),
+			}),
+		}).
 		Create(s).
 		Error
 
