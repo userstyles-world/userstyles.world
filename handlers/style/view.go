bffdf6281c983d9f8a1d74cd7eddf881224d91c5
vednoc <vednoc@protonmail.com>
2021-04-11 23:47:45 +0200

    feat: show Style weekly/total install counters
    Fixes #45.

--- handlers/style/view.go
+++ handlers/style/view.go
@@ -2,6 +2,7 @@ package style
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 
@@ -12,8 +13,9 @@ import (
 
 func GetStyle(c *fiber.Ctx) error {
 	u, _ := jwt.User(c)
+	id := c.Params("id")
 
-	data, err := models.GetStyleByID(database.DB, c.Params("id"))
+	data, err := models.GetStyleByID(database.DB, id)
 	if err != nil {
 		return c.Render("err", fiber.Map{
 			"Title": "Style not found",
@@ -21,10 +23,26 @@ func GetStyle(c *fiber.Ctx) error {
 		})
 	}
 
+	var total int64
+	database.DB.
+		Debug().
+		Model(models.Stats{}).
+		Where("style_id = ?", id).
+		Count(&total)
+
+	var week int64
+	database.DB.
+		Debug().
+		Model(models.Stats{}).
+		Where("style_id = ? and updated_at > ?", id, time.Now().AddDate(0, 0, -7)).
+		Count(&week)
+
 	return c.Render("style", fiber.Map{
 		"Title": data.Name,
 		"User":  u,
 		"Style": data,
+		"Total": total,
+		"Week":  week,
 		"Url":   fmt.Sprintf("https://userstyles.world/style/%d", data.ID),
 	})
 }
