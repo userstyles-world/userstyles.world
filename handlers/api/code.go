19ddae705f41957ca592e43cb1cadbe86c06f95a
Gusted <williamzijl7@hotmail.com>
2021-04-14 22:25:57 +0200

    refactor: use md5 for Etag

--- handlers/api/code.go
+++ handlers/api/code.go
@@ -1,14 +1,14 @@
 package api
 
 import (
-	"hash/crc32"
+	"crypto/md5"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/vednoc/go-usercss-parser"
 
 	"userstyles.world/database"
 	"userstyles.world/models"
-	"userstyles.world/utils"
 )
 
 func GetStyleSource(c *fiber.Ctx) error {
@@ -48,31 +48,11 @@ func GetStyleEtag(c *fiber.Ctx) error {
 	}
 
 	// TODO: add a possible update stat?
-	// TODO: internal switch to a byte pool to avoid allocations over time.
-	etagValue := make([]byte, 0, 40)
-	etagValue = appendUint(etagValue, uint32(len(style.Code)))
-	etagValue = append(etagValue, '-')
-	etagValue = appendUint(etagValue, crc32.ChecksumIEEE(utils.S2b(style.Code)))
 
-	c.Response().Header.SetCanonical(normalizedHeaderETag, etagValue)
+	// Follows the format "source code length - MD5 Checksum of source code"
+	etagValue := fmt.Sprintf("\"%d-%x\"", len(style.Code), md5.Sum([]byte(style.Code)))
+
+	// Set the value for "Etag" header
+	c.Response().Header.SetCanonical(normalizedHeaderETag, []byte(etagValue))
 	return nil
-}
-
-// appendUint appends n to dst and returns the extended dst.
-func appendUint(dst []byte, n uint32) []byte {
-	var b [20]byte
-	buf := b[:]
-	i := len(buf)
-	var q uint32
-	for n >= 10 {
-		i--
-		q = n / 10
-		buf[i] = '0' + byte(n-q*10)
-		n = q
-	}
-	i--
-	buf[i] = '0' + byte(n)
-
-	dst = append(dst, buf[i:]...)
-	return dst
 }
