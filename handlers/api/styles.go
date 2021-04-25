9c952277c9166f1123bb9a7794d1ce1e73d64908
Gusted <williamzijl7@hotmail.com>
2021-04-26 00:49:25 +0200

    feat: add style

--- handlers/api/styles.go
+++ handlers/api/styles.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/ohler55/ojg/oj"
+	"github.com/vednoc/go-usercss-parser"
 	"userstyles.world/database"
 	"userstyles.world/models"
 	"userstyles.world/utils"
@@ -122,5 +123,68 @@ func DeleteStyle(c *fiber.Ctx) error {
 
 	return c.JSON(fiber.Map{
 		"data": "Succesful removed the style!",
+	})
+}
+
+func NewStyle(c *fiber.Ctx) error {
+	u, _ := APIUser(c)
+
+	if !utils.Contains(u.Scopes, "style") {
+		return c.Status(403).
+			JSON(fiber.Map{
+				"data": "You need the \"style\" scope to do this.",
+			})
+	}
+
+	var postStyle models.Style
+	err := JsonParser.Unmarshal(c.Body(), &postStyle)
+	if err != nil {
+		fmt.Println(err)
+		return c.Status(500).
+			JSON(fiber.Map{
+				"data": "Error: Couldn't convert style to struct.",
+			})
+	}
+
+	if postStyle.Name == "" || postStyle.Code == "" || postStyle.Description == "" || postStyle.Category == "" {
+		return c.Status(403).
+			JSON(fiber.Map{
+				"data": "Make sure to fill out fields.",
+			})
+	}
+	postStyle.Featured = false
+	postStyle.UserID = u.ID
+
+	code := usercss.ParseFromString(postStyle.Code)
+	if errs := usercss.BasicMetadataValidation(code); errs != nil {
+		var errors string
+		for i := 0; i < len(errs); i++ {
+			errors += errs[i].Code.Error() + ";"
+		}
+		return c.Status(403).
+			JSON(fiber.Map{
+				"data": "Error:" + errors,
+			})
+	}
+
+	// Prevent adding multiples of the same style.
+	err = models.CheckDuplicateStyle(database.DB, &postStyle)
+	if err != nil {
+		return c.Status(403).
+			JSON(fiber.Map{
+				"data": "A duplicate style was found.",
+			})
+	}
+
+	newStyle, err := models.CreateStyle(database.DB, &postStyle)
+	if err != nil {
+		return c.Status(500).
+			JSON(fiber.Map{
+				"data": "Error: Couldn't save style",
+			})
+	}
+
+	return c.JSON(fiber.Map{
+		"data": "Succesful added the style! With ID: " + fmt.Sprintf("%d", newStyle.ID),
 	})
 }
