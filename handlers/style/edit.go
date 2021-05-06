3d0a02b576541f63f3cf8f905d787bf801e5a852
Gusted <williamzijl7@hotmail.com>
2021-05-06 22:58:15 +0200

    feat: allow image edit

--- handlers/style/edit.go
+++ handlers/style/edit.go
@@ -1,13 +1,18 @@
 package style
 
 import (
+	"io"
 	"log"
+	"mime/multipart"
+	"os"
+	"strconv"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
 
 	"userstyles.world/database"
 	"userstyles.world/handlers/jwt"
+	"userstyles.world/images"
 	"userstyles.world/models"
 )
 
@@ -55,7 +60,37 @@ func StyleEditPost(c *fiber.Ctx) error {
 		UserID:      u.ID,
 	}
 
-	err := database.DB.
+	var image multipart.File
+	var err error
+
+	if ff, _ := c.FormFile("preview"); ff != nil {
+		image, err = ff.Open()
+		if err != nil {
+			log.Println("Opening image , err:", err)
+			return c.Render("err", fiber.Map{
+				"Title": "Internal server error.",
+				"User":  u,
+			})
+		}
+	}
+
+	if image != nil {
+		ID := strconv.FormatUint(uint64(u.ID), 10)
+		data, _ := io.ReadAll(image)
+		err = os.WriteFile(images.CacheFolder+ID+".original", data, 0644)
+		_ = os.Remove(images.CacheFolder + ID + ".jpeg")
+		_ = os.Remove(images.CacheFolder + ID + ".webp")
+		if err != nil {
+			log.Println("Style creation failed, err:", err)
+			return c.Render("err", fiber.Map{
+				"Title": "Internal server error.",
+				"User":  u,
+			})
+		}
+		q.Preview = "https://userstyles.world/api/preview/" + ID + ".jpeg"
+	}
+
+	err = database.DB.
 		Model(t).
 		Where("id", p).
 		Updates(q).
