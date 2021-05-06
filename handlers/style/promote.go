72b22004124c828e7afd7123ac1e5d9fa7ae77c8
Gusted <williamzijl7@hotmail.com>
2021-05-06 23:29:21 +0200

    fix: only allow moderator+ to promote

--- handlers/style/promote.go
+++ handlers/style/promote.go
@@ -12,6 +12,14 @@ func StylePromote(c *fiber.Ctx) error {
 	u, _ := jwt.User(c)
 	p := c.Params("id")
 
+	// Only moderator and above have permissions to promote styles.
+	if u.Role < models.Moderator {
+		return c.Render("err", fiber.Map{
+			"Title": "You don't have enough permission for this.",
+			"User":  u,
+		})
+	}
+
 	// TODO: Make it possible to remove promotion.
 	err := database.DB.
 		Model(models.Style{}).
