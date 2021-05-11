83ed5e9c887945931dd76416ac990f6372d42ba6
Gusted <williamzijl7@hotmail.com>
2021-05-11 20:54:34 +0200

    refactor: clean up legal.go

--- handlers/core/legal.go
+++ handlers/core/legal.go
@@ -9,30 +9,21 @@ import (
 func GetLegal(c *fiber.Ctx) error {
 	document := c.Params("document")
 
+	var content []byte
 	switch document {
 	case "privacy-policy":
-		content, err := os.ReadFile("privacy-policy.md")
-		if err != nil {
-			c.Render("err", fiber.Map{
-				"Title": "Couldn't load the document.",
-			})
-		}
-		return c.Render("legal", fiber.Map{
-			"content": string(content),
-		})
+		content, _ = os.ReadFile("privacy-policy.md")
 	case "terms-of-service":
-		content, err := os.ReadFile("terms-of-service.md")
-		if err != nil {
-			c.Render("err", fiber.Map{
-				"Title": "Couldn't load the document.",
-			})
-		}
-		return c.Render("legal", fiber.Map{
-			"content": string(content),
+		content, _ = os.ReadFile("terms-of-service.md")
+	}
+
+	if len(content) == 0 {
+		return c.Render("err", fiber.Map{
+			"Title": "Couldn't load the document.",
 		})
 	}
 
-	return c.Render("err", fiber.Map{
-		"Title": "Couldn't load the document.",
+	return c.Render("legal", fiber.Map{
+		"content": string(content),
 	})
 }
