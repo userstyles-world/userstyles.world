9bec8b52b40f73062fc9b12de61fc3e6ac4f6688
vednoc <vednoc@protonmail.com>
2021-05-15 18:36:36 +0200

    fix: show user state on docs route

--- handlers/core/docs.go
+++ handlers/core/docs.go
@@ -4,9 +4,12 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+
+	"userstyles.world/handlers/jwt"
 )
 
 func GetDocs(c *fiber.Ctx) error {
+	u, _ := jwt.User(c)
 	document := c.Params("document")
 
 	var content []byte
@@ -18,11 +21,13 @@ func GetDocs(c *fiber.Ctx) error {
 	if len(content) == 0 {
 		return c.Render("err", fiber.Map{
 			"Title": "Couldn't load the document.",
+			"User":  u,
 		})
 	}
 
 	return c.Render("docs", fiber.Map{
 		"Title":   "Changelog",
+		"User":    u,
 		"content": string(content),
 	})
 }
