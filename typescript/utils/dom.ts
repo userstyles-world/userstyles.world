224c3d49c364f6445deaa77cf862ad148305ec4a
Gusted <williamzijl7@hotmail.com>
2021-05-17 19:12:43 +0200

    fix(typescript): yoda-compatible

--- typescript/utils/dom.ts
+++ typescript/utils/dom.ts
@@ -1,6 +1,6 @@
 const readyStateListeners = new Set<() => void>();
 
-export const isDOMReady = () => document.readyState === 'complete' || document.readyState === 'interactive';
+export const isDOMReady = () => 'complete' === document.readyState || 'interactive' === document.readyState;
 export const addDOMReadyListener = (listener: () => void) => readyStateListeners.add(listener);
 
 if (!isDOMReady()) {
