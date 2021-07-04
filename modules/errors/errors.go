8dd828af12433523839ca4e92502446970adb3a3
Gusted <williamzijl7@hotmail.com>
2021-07-04 20:18:54 +0200

    refactor(errors): don't capitalize

--- modules/errors/errors.go
+++ modules/errors/errors.go
@@ -66,11 +66,11 @@ var (
 	errUnexpectedSigningMethod = errors.New("unexpected jwt signing method")
 
 	// ErrVipsNotFound errors that the `which vips` command couldn't vips in the current PATH.
-	ErrVipsNotFound = errors.New("The vips binary wasn't found")
+	ErrVipsNotFound = errors.New("the vips binary wasn't found")
 
 	// ErrNoImageProcessing errors that the vips CLI returned a error or a non-zero return code.
 	// meaning that it failed to do the task.
-	ErrNoImageProcessing = errors.New("The image processor couldn't process the image")
+	ErrNoImageProcessing = errors.New("the image processor couldn't process the image")
 )
 
 // UnexpectedSigningMethod errors that a unexpected jwt signing method was used.
