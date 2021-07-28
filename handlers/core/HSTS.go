d8a3b925a7b66d7750cf1a2635321fc6f43a8209
Gusted <williamzijl7@hotmail.com>
2021-07-29 01:07:17 +0200

    refactor(handlers): make variables more specici

--- handlers/core/HSTS.go
+++ handlers/core/HSTS.go
@@ -3,16 +3,16 @@ package core
 import "github.com/gofiber/fiber/v2"
 
 var (
-	header = []byte("Strict-Transport-Security")
+	headerHSTS = []byte("Strict-Transport-Security")
 	// Make it so that that the max-age is 2 years, it's high enough.
 	// We also say that every subdomain has HTTPS://
 	// And we say that we are preload(on browser's list).
-	value = []byte("max-age=63072000; includeSubDomains; preload")
+	valueHSTS = []byte("max-age=63072000; includeSubDomains; preload")
 )
 
 // HSTSMiddleware adds the HSTS Header
 // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
 func HSTSMiddleware(c *fiber.Ctx) error {
-	c.Response().Header.SetCanonical(header, value)
+	c.Response().Header.SetCanonical(headerHSTS, valueHSTS)
 	return c.Next()
 }
