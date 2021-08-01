361bd092f7c452b3316a8e867140a0fe17a62b28
Gusted <williamzijl7@hotmail.com>
2021-08-01 16:31:32 +0200

    tests(core): add test case for CSP

--- handlers/core/CSP_test.go
+++ handlers/core/CSP_test.go
@@ -29,6 +29,54 @@ func TestCSPMiddlewareCorrect(t *testing.T) {
 	if res.StatusCode != fiber.StatusOK {
 		t.Error("Expected 200, got ", res.StatusCode)
 	}
+	if res.Header.Get(string(headerCSP)) != string(valueCSPStrictForm) {
+		t.Error("\nExpected ", string(valueCSPStrictForm), "\ngot ", res.Header.Get(string(headerCSP)))
+	}
+}
+
+// Because of some unexepected behavior.
+// In the CSP Policy on any page that starts with /api/oauth/style/.
+// It will add an exception to the form-action CSP directive.
+func TestCSPException(t *testing.T) {
+	t.Parallel()
+
+	app := fiber.New()
+	app.Use(CSPMiddleware)
+	app.Get("/api/oauth/style/new", func(c *fiber.Ctx) error {
+		c.Response().Header.SetContentType(fiber.MIMETextHTMLCharsetUTF8)
+		return c.SendString("<p>Hi, wanna add a style?</p>")
+	})
+	app.Get("/api/oauth/style/link", func(c *fiber.Ctx) error {
+		c.Response().Header.SetContentType(fiber.MIMETextHTMLCharsetUTF8)
+		return c.SendString("<p>Hi linking?</p>")
+	})
+
+	req, err := http.NewRequest("GET", "/api/oauth/style/new", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	res, err := app.Test(req)
+	if err != nil {
+		t.Error(err)
+	}
+	if res.StatusCode != fiber.StatusOK {
+		t.Error("Expected 200, got ", res.StatusCode)
+	}
+	if res.Header.Get(string(headerCSP)) != string(valueCSP) {
+		t.Error("\nExpected ", string(valueCSP), "\ngot ", res.Header.Get(string(headerCSP)))
+	}
+
+	req, err = http.NewRequest("GET", "/api/oauth/style/link", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	res, err = app.Test(req)
+	if err != nil {
+		t.Error(err)
+	}
+	if res.StatusCode != fiber.StatusOK {
+		t.Error("Expected 200, got ", res.StatusCode)
+	}
 	if res.Header.Get(string(headerCSP)) != string(valueCSP) {
 		t.Error("\nExpected ", string(valueCSP), "\ngot ", res.Header.Get(string(headerCSP)))
 	}
