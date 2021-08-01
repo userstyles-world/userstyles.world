badf62bc8b1d64af966ac8318ae62bdf23a255e9
Gusted <williamzijl7@hotmail.com>
2021-08-01 16:59:06 +0200

    tests(core): add errors_test.go

--- handlers/core/errors.go
+++ handlers/core/errors.go
@@ -9,14 +9,14 @@ import (
 )
 
 func NotFound(c *fiber.Ctx) error {
-	u, _ := jwt.User(c)
-
+	c.Status(404)
 	// Handle 404 errors on API route, otherwise render error template.
 	if strings.HasPrefix(c.OriginalURL(), "/api/") {
 		return c.JSON(fiber.Map{
 			"error": "bad endpoint",
 		})
 	}
+	u, _ := jwt.User(c)
 	return c.Render("err", fiber.Map{
 		"User":      u,
 		"Title":     "Page not found",
---
+++ handlers/core/errors_test.go
@@ -0,0 +1,102 @@
+package core
+
+import (
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"userstyles.world/modules/templates"
+)
+
+// Check if you get the correct 404 page.
+// Depending on the request.
+func Test404Normal(t *testing.T) {
+	t.Parallel()
+
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		c.Response().Header.SetContentType(fiber.MIMETextHTMLCharsetUTF8)
+		return c.SendString("<p>Hi, wanna have some fun</p>")
+	})
+	app.Get("/api/index", func(c *fiber.Ctx) error {
+		c.Response().Header.SetContentType(fiber.MIMEApplicationJSONCharsetUTF8)
+		return c.SendString(`{"name":"fiber"}`)
+	})
+	app.Use(NotFound)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	res, err := app.Test(req)
+	if err != nil {
+		t.Error(err)
+	}
+	if res.StatusCode != fiber.StatusOK {
+		t.Error("Expected 200, got ", res.StatusCode)
+	}
+
+	req, err = http.NewRequest("GET", "/api/index", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	res, err = app.Test(req)
+	if err != nil {
+		t.Error(err)
+	}
+	if res.StatusCode != fiber.StatusOK {
+		t.Error("Expected 200, got ", res.StatusCode)
+	}
+}
+
+func Test404Pages(t *testing.T) {
+	t.Parallel()
+
+	app := fiber.New(fiber.Config{
+		Views: templates.New(),
+	})
+	app.Get("/", func(c *fiber.Ctx) error {
+		c.Response().Header.SetContentType(fiber.MIMETextHTMLCharsetUTF8)
+		return c.SendString("<p>Hi, wanna have some fun</p>")
+	})
+	app.Get("/api/index", func(c *fiber.Ctx) error {
+		c.Response().Header.SetContentType(fiber.MIMEApplicationJSONCharsetUTF8)
+		return c.SendString(`{"name":"fiber"}`)
+	})
+	app.Use(NotFound)
+
+	req, err := http.NewRequest("GET", "/api/notfound", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	res, err := app.Test(req)
+	if err != nil {
+		t.Error(err)
+	}
+	if res.StatusCode != fiber.StatusNotFound {
+		t.Error("Expected 404, got ", res.StatusCode)
+	}
+
+	expected := `{"error":"bad endpoint"}`
+	body := make([]byte, len(expected))
+	_, err = res.Body.Read(body)
+	if err != nil && err != io.EOF {
+		t.Error(err)
+	}
+	if string(body) != expected {
+		t.Error("Expected ", expected, " got ", string(body))
+	}
+
+	req, err = http.NewRequest("GET", "/notfound", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	res, err = app.Test(req)
+	if err != nil {
+		t.Error(err)
+	}
+	if res.StatusCode != fiber.StatusNotFound {
+		t.Error("Expected 404, got ", res.StatusCode)
+	}
+}
