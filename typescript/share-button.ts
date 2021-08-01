0ca4543e58f74c4e85cb2d2ef36bd57593cb2cd7
Gusted <williamzijl7@hotmail.com>
2021-08-01 03:54:33 +0200

    fix(ts): correct copy mechanism for span element
    Thank you @FreeplayG for reminding me to fix this.

--- typescript/share-button.ts
+++ typescript/share-button.ts
@@ -1,13 +1,18 @@
 export function ShareButton() {
-    const parentElement = document.getElementById('share') as HTMLInputElement;
+    const parentElement = document.getElementById('share') as HTMLSpanElement;
     const shareButton = document.getElementById('btn-share') as HTMLButtonElement;
     if (!shareButton) {
         return;
     }
     shareButton.addEventListener('click', () => {
-        parentElement.select();
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(parentElement);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        // add to clipboard.
         document.execCommand('copy');
-        parentElement.blur();
+
         shareButton.classList.add('copied');
     });
 }
