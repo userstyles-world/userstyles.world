7735365c9810c77425029bc15ff7cf1e6acd4db4
vednoc <vednoc@protonmail.com>
2021-08-03 22:47:59 +0200

    feat(api): speed up USo-format endpoint

--- handlers/api/index.go
+++ handlers/api/index.go
@@ -9,10 +9,13 @@ import (
 	"github.com/patrickmn/go-cache"
 
 	"userstyles.world/models"
+	"userstyles.world/modules/database"
+	"userstyles.world/modules/errors"
+	"userstyles.world/modules/log"
 )
 
 type USoFormat struct {
-	Author         string `json:"an"`
+	Username       string `json:"an"`
 	Name           string `json:"n"`
 	Category       string `json:"c"`
 	Screenshot     string `json:"sn"`
@@ -20,6 +23,31 @@ type USoFormat struct {
 	TotalInstalls  int64  `json:"t"`
 	WeeklyInstalls int64  `json:"w"`
 	ID             uint   `json:"i"`
+}
+
+type USoStyles []USoFormat
+
+func (s *USoStyles) Query() error {
+	lastWeek := time.Now().Add(-time.Hour * 24 * 7)
+
+	stmt := "styles.id, styles.name, styles.user_id, u.username, "
+	stmt += "styles.category, strftime('%s', styles.created_at) as created_at, "
+	stmt += "strftime('%s', styles.updated_at) as updated_at, "
+	stmt += "printf('https://userstyles.world/api/style/preview/%d.webp', styles.id) as screenshot, "
+	stmt += "(select count(*) from stats where stats.style_id = styles.id) as total_installs, "
+	stmt += "(select count(*) from stats where stats.style_id = styles.id and updated_at > ? and created_at < ?) as weekly_installs"
+
+	err := database.Conn.
+		Table("styles").
+		Select(stmt, lastWeek, lastWeek).
+		Joins("join users u on u.id = styles.user_id").
+		Find(&s).
+		Error
+	if err != nil {
+		return errors.ErrStylesNotFound
+	}
+
+	return nil
 }
 
 var mem = cache.New(5*time.Minute, 10*time.Minute)
@@ -36,7 +64,7 @@ func convertToUSoFormat(s models.APIStyle) USoFormat {
 		ID:             s.ID,
 		Name:           s.Name,
 		Category:       fixCategory(s.Category),
-		Author:         s.Username,
+		Username:       s.Username,
 		Screenshot:     img,
 		UpdatedAt:      s.UpdatedAt.Unix(),
 		TotalInstalls:  models.GetTotalInstallsForStyle(id),
@@ -69,19 +97,20 @@ func getUSoIndex(c *fiber.Ctx) error {
 Convert:
 	cached, found := mem.Get("index")
 	if !found {
-		styles, err := models.GetAllStylesForIndexAPI()
-		if err != nil {
+		styles := new(USoStyles)
+		if err := styles.Query(); err != nil {
+			log.Warn.Fatal("API/index/uso-format err:", err.Error())
 			return c.JSON(fiber.Map{
 				"data": "styles not found",
 			})
 		}
 
-		formatted := make([]USoFormat, len(*styles))
-		for i, style := range *styles {
-			formatted[i] = convertToUSoFormat(style)
+		// TODO: Normalize categories on add/import/edit pages.
+		for _, style := range *styles {
+			style.Category = fixCategory(style.Category)
 		}
 
-		mem.Set("index", formatted, 10*time.Minute)
+		mem.Set("index", styles, 10*time.Minute)
 		goto Convert
 	}
 
