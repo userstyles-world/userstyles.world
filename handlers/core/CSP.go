56c6dfb78421adc4afd528f6bdbd4865ca400b5d
Gusted <williamzijl7@hotmail.com>
2021-08-08 20:54:20 +0200

    feat(tools): add 'fonts' command to run tool (#76)
    * chore: don't use third party for fonts
    * chore(static): update Inter font to latest version
    * feat(tools): add 'fonts' command to run tool
    * chore(static): remove unnecessary font files
    
    Co-authored-by: vednoc <vednoc@protonmail.com>

--- handlers/core/CSP.go
+++ handlers/core/CSP.go
@@ -10,7 +10,7 @@ var (
 	headerCSP = []byte(fiber.HeaderContentSecurityPolicy)
 
 	valueCSPStrictForm = append(valueCSP, []byte(" form-action 'self';")...)
-	valueCSP           = []byte("default-src 'none'; font-src https://fonts.imma.link; img-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors 'none'; upgrade-insecure-requests; base-uri 'none'; object-src 'none'; worker-src 'none'; child-src 'none'; frame-src 'none'; connect-src 'self';")
+	valueCSP           = []byte("default-src 'none'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors 'none'; upgrade-insecure-requests; base-uri 'none'; object-src 'none'; worker-src 'none'; child-src 'none'; frame-src 'none'; connect-src 'self';")
 )
 
 // CSPMiddleware adds the CSP Header
