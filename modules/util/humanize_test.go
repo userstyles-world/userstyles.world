116e4d4eabcaaed93c52323a43c6b9274e242878
vednoc <vednoc@protonmail.com>
2023-01-02 10:34:32 +0100

    test(util): add benchmark for `RelNumber` function

--- modules/util/humanize_test.go
+++ modules/util/humanize_test.go
@@ -2,27 +2,37 @@ package util
 
 import "testing"
 
+var relNumberCases = []struct {
+	name     string
+	input    int
+	expected string
+}{
+	{"hundreds of thousands", 777777, "777.7k"},
+	{"tens of thousands", 42069, "42k"},
+	{"thousands", 1337, "1.3k"},
+	{"hundreds", 420, "420"},
+	{"tens", 42, "42"},
+}
+
 func TestRelNumber(t *testing.T) {
 	t.Parallel()
 
-	cases := []struct {
-		name     string
-		input    int
-		expected string
-	}{
-		{"Hundreds of thousands", 777777, "777.7k"},
-		{"Tens of thousands", 42069, "42k"},
-		{"Thousands", 1337, "1.3k"},
-		{"Hundreds", 420, "420"},
-		{"Tens", 42, "42"},
-	}
-
-	for _, c := range cases {
+	for _, c := range relNumberCases {
 		t.Run(c.name, func(t *testing.T) {
 			got := RelNumber(c.input)
 			if got != c.expected {
 				t.Errorf("got: %s\n", got)
 				t.Errorf("exp: %s\n", c.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkRelNumber(b *testing.B) {
+	for _, c := range relNumberCases {
+		b.Run(c.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				RelNumber(c.input)
 			}
 		})
 	}
