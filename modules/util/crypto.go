a0dbb63e6b806aabf627b390d9ccf6e8b1e9ea15
vednoc <vednoc@protonmail.com>
2023-01-26 10:36:05 +0100

    perf(util): optimize `HashIP` function
    The 2 leftover allocations seem to be from the underlying machinery, but
    I'm guesstimating here since I didn't do more than skimming through the
    source code.  I'm happy putting 20% of effort for 80% of the results.
    
    ```
    benchmark                     old ns/op     new ns/op     delta
    BenchmarkHashIP/foobar-8      3989          1586          -60.24%
    BenchmarkHashIP/real_IP-8     4102          1600          -60.99%
    BenchmarkHashIP/no_IP-8       4287          1589          -62.93%
    BenchmarkHashIP/no_ID-8       4255          1607          -62.23%
    
    benchmark                     old allocs     new allocs     delta
    BenchmarkHashIP/foobar-8      10             2              -80.00%
    BenchmarkHashIP/real_IP-8     10             2              -80.00%
    BenchmarkHashIP/no_IP-8       10             2              -80.00%
    BenchmarkHashIP/no_ID-8       10             2              -80.00%
    
    benchmark                     old bytes     new bytes     delta
    BenchmarkHashIP/foobar-8      1160          72            -93.79%
    BenchmarkHashIP/real_IP-8     1168          80            -93.15%
    BenchmarkHashIP/no_IP-8       1160          72            -93.79%
    BenchmarkHashIP/no_ID-8       1168          80            -93.15%
    ```

--- modules/util/crypto.go
+++ modules/util/crypto.go
@@ -4,16 +4,42 @@ import (
 	"crypto/hmac"
 	"crypto/sha512"
 	"encoding/hex"
+	"hash"
+	"sync"
+	"unsafe"
 
 	"userstyles.world/modules/config"
 )
 
+var (
+	bufHashPool = sync.Pool{
+		New: func() interface{} {
+			b := make([]byte, 128)
+			return &b
+		},
+	}
+	hmacPool = sync.Pool{
+		New: func() interface{} {
+			return hmac.New(sha512.New, []byte(config.StatsKey))
+		},
+	}
+)
+
 // HashIP generates a unique hash for stats.
 func HashIP(key string) (string, error) {
-	h := hmac.New(sha512.New, []byte(config.StatsKey))
+	bp := bufHashPool.Get().(*[]byte)
+	defer bufHashPool.Put(bp)
+	b := *bp
+
+	h := hmacPool.Get().(hash.Hash)
+	defer hmacPool.Put(h)
+	h.Reset()
+
 	if _, err := h.Write([]byte(key)); err != nil {
 		return "", err
 	}
 
-	return hex.EncodeToString(h.Sum(nil)), nil
+	hex.Encode(b, h.Sum(nil))
+
+	return *(*string)(unsafe.Pointer(&b)), nil
 }
