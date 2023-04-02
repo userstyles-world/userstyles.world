90ded4e8e270ca521bd4f78e6d50be3b3c073125
vednoc <vednoc@protonmail.com>
2023-04-02 17:36:02 +0200

    fix(index): add back USo-style rating calculation
    Thank you @a0eoc for finding and reporting this bug.

--- modules/storage/style_api.go
+++ modules/storage/style_api.go
@@ -11,14 +11,17 @@ import (
 	"userstyles.world/modules/errors"
 )
 
-var (
+const (
 	selectWeeklyInstalls = "(SELECT COUNT(*) FROM stats s WHERE s.style_id = styles.id AND s.install > DATETIME('now', '-7 days') AND s.created_at > DATETIME('now', '-7 days')) AS WeeklyInstalls"
 	selectTotalInstalls  = "(SELECT COUNT(*) FROM stats s WHERE s.style_id = styles.id AND s.install > 0) AS TotalInstalls"
+	selectUSoRatings     = "(SELECT ROUND(AVG(rating)*0.6, 1) FROM reviews r WHERE r.style_id = styles.id AND r.deleted_at IS NULL) AS Rating"
+)
 
+var (
 	selectCompactIndex = strings.Join([]string{
 		"styles.id", "styles.name", "styles.preview", "styles.category",
 		"STRFTIME('%s', styles.updated_at) AS updated_at",
-		selectAuthor, selectTotalInstalls, selectWeeklyInstalls, selectRatings,
+		selectAuthor, selectTotalInstalls, selectWeeklyInstalls, selectUSoRatings,
 	}, ", ")
 )
 
