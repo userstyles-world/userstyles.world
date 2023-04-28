9494690727a19c1ee70a331c50cf090505d71036
vednoc <vednoc@protonmail.com>
2023-04-28 17:55:39 +0200

    refactor(tmpl): simplify code for search page

--- handlers/core/search.go
+++ handlers/core/search.go
@@ -13,60 +13,45 @@ import (
 
 func Search(c *fiber.Ctx) error {
 	u, _ := jwt.User(c)
+	c.Locals("User", u)
+	c.Locals("Title", "Search userstyles")
+	c.Locals("Canonical", "search")
 
 	keyword := c.Query("q")
 	if keyword == "" {
-		return c.Render("core/search", fiber.Map{
-			"Title": "Search",
-			"User":  u,
-		})
+		return c.Render("core/search", fiber.Map{})
 	}
+	c.Locals("Keyword", keyword)
 
 	page, err := models.IsValidPage(c.Query("page"))
 	if err != nil || page < 1 {
-		return c.Status(fiber.StatusBadRequest).Render("err", fiber.Map{
-			"Title": "Invalid page size",
-			"User":  u,
-		})
+		c.Locals("Title", "Invalid page size")
+		return c.Status(fiber.StatusBadRequest).Render("err", fiber.Map{})
 	}
 
 	sort := c.Query("sort")
+	c.Locals("Sort", sort)
 
 	s, m, err := search.FindStylesByText(keyword, sort, page, config.AppPageMaxItems)
-	if errors.Is(err, search.ErrSearchNoResults) {
-		return c.
-			Status(fiber.StatusNotFound).
-			Render("core/search", fiber.Map{
-				"Title":   "No results found",
-				"User":    u,
-				"Keyword": keyword,
-				"Sort":    sort,
-				"Error":   "No results found for <b>" + keyword + "</b>.",
-			})
-	} else if err != nil {
-		return c.
-			Status(fiber.StatusBadRequest).
-			Render("core/search", fiber.Map{
-				"User":  u,
-				"Title": "Bad search request",
-				"Error": "Bad search request.",
-			})
+	switch {
+	case errors.Is(err, search.ErrSearchNoResults):
+		c.Locals("Title", "No results found")
+		c.Locals("Error", "No results found for <b>"+keyword+"</b>.")
+		return c.Status(fiber.StatusNotFound).Render("core/search", fiber.Map{})
+	case err != nil:
+		c.Locals("Title", "Bad search request")
+		c.Locals("Error", "Bad search request. Please try again.")
+		return c.Status(fiber.StatusBadRequest).Render("core/search", fiber.Map{})
 	}
+	c.Locals("Styles", s)
+	c.Locals("Metrics", m)
 
 	p := models.NewPagination(page, m.Total, sort, c.Path())
 	p.Query = keyword
 	if p.OutOfBounds() {
 		return c.Redirect(p.URL(p.Now), 302)
 	}
+	c.Locals("Pagination", p)
 
-	return c.Render("core/search", fiber.Map{
-		"Title":     "Search",
-		"User":      u,
-		"Styles":    s,
-		"Keyword":   keyword,
-		"Sort":      p.Sort,
-		"Canonical": "search",
-		"Metrics":   m,
-		"P":         p,
-	})
+	return c.Render("core/search", fiber.Map{})
 }
