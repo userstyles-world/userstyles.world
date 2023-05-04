5595625cc2501e9b5dc468be75f5eb6c3bbadd09
θ̫͖͑͋ε̧̺̊̄ó̪̱̒͝ς <42794343+a0eoc@users.noreply.github.com>
2023-05-04 17:26:57 +0500

    remove console.log

--- web/typescript/page/edit.ts
+++ web/typescript/page/edit.ts
@@ -1,10 +1,8 @@
 export function checkMaxLength() {
     [...document.querySelectorAll('textarea[maxlength]')].forEach((element) => {
         const maxlength = element.getAttribute('maxlength');
-        console.log(maxlength)
         element.removeAttribute('maxlength');
         element.addEventListener('input', () => {
-            console.log(element.value.length)
             element.setCustomValidity(maxlength >= element.value.length ? '' : 'Your imput must be shorter than ' + maxlength + ' characters.');
         }, {passive: true});
     });
