8da559080c82b1aa31e23be4301c2578867c3365
θ̫͖͑͋ε̧̺̊̄ó̪̱̒͝ς <42794343+a0eoc@users.noreply.github.com>
2023-05-17 23:19:34 +0500

    changes

--- tools/licenser_postprocess.py
+++ tools/licenser_postprocess.py
@@ -3,16 +3,19 @@ This script generates the license documentation pages.
 """
 
 from os import system, getcwd, listdir, chmod, rename
-from os.path import dirname, basename
+from os.path import join, dirname, basename
+from shutil import copyfile
+
+# Directory where the missing licenses are.
+CustomDir = join(dirname(__file__), "../web/customlicenses" )
 
 # Directory where the scraped licenses are.
-Dir = dirname(__file__) + "/../web/"
+Dir = join(dirname(__file__), "../web/docs/licenses")
 
 # Copy the licenses from customlicenses to licenses documentation.
-Licenses = listdir(Dir + "customlicenses")
+Licenses = listdir(CustomDir)
 for License in Licenses:
-	system("cp " + Dir + "customlicenses/" + License + " " + Dir + "docs/licenses/" + License)
-Dir += "docs/licenses"
+	copyfile(join(CustomDir, License), join(Dir, License))
 
 IndexPath = Dir + ".md"
 IndexText = ""
@@ -22,13 +25,14 @@ Licenses = listdir(Dir)
 for License in Licenses:
 	Path = Dir + "/" + License
 
-	# make files writeable
+	# Make files writeable, as they may not be.
 	chmod(Path, 0o0600)
+
 	# Read the license content and add it as a codeblock.
 	with open(Path, "r") as File:
-		TheText = "```\n" + File.read().strip() + "\n```"
+		LicenseText = File.read().strip()
 
-	# use filename as license name, remove host prefix
+	# Use filename as license name, remove host prefix.
 	Name = License.partition("-")[2].removesuffix(".txt")
 	RelName = Name
 	Name += " license"
@@ -36,20 +40,24 @@ for License in Licenses:
 	# Generate the document, which has the following: the title, a link to the index file and the license content.
 	Text = f"---\nTitle: {Name}\n---\n# {Name}"
 	Text += "\n\n[Back to licenses](/docs/licenses)\n\n"
-	Text += "\n" + TheText
+	Text += "\n```\n" + LicenseText + "\n```"
 
-	if RelName == "stylus-logo":
-		Text += "\n\n\n#### Changes:\n- vectorized by [pabli24](https://github.com/pabli24)\n- converted to monochrome outline by [0eoc](https://userstyles.world/user/0eoc)"
+	# Optionally specify the changes for GPL-licensed dependencies.
+	ChangedDependencies = {
+		"stylus-logo": "\n- vectorized by [pabli24](https://github.com/pabli24)\n- converted to monochrome outline by [0eoc](https://userstyles.world/user/0eoc)",
+	}
+	if RelName in ChangedDependencies:
+		Text += "\n\n\n#### Changes:" + ChangedDependencies[RelName]
 
 	with open(Path, "w") as File:
 		File.write(Text)
 
-	# rename file
+	# Change the file extension to the correct one.
 	rename(Path, Dir + "/" + RelName + ".md")
 
 	# Add this license to the index page.
-	IndexText += "\n- [" + RelName + "](/docs/licenses/" + RelName + ")\n"
+	IndexText += f"\n- [{RelName}](/docs/licenses/{RelName})\n"
 
-# write license index. we could write to it on every license but i prefer writing big chunks
+# Write the license index. We could write to it on every license, but for performance reasons, it's preferred to write to it all at once.
 with open(IndexPath, "w") as IndexFile:
 	IndexFile.write(f"---\nTitle: Third-party Licenses\n---\n# Third-party Licenses\n{IndexText}")
