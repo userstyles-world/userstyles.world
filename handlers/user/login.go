3bb28456b6d63b7056befcd36a30b5d91e992525
θ̫͖͑͋ε̧̺̊̄ó̪̱̒͝ς <42794343+a0eoc@users.noreply.github.com>
2023-05-27 22:35:57 +0500

    feedback + log-in

--- handlers/user/login.go
+++ handlers/user/login.go
@@ -125,23 +125,23 @@ func LoginPost(c *fiber.Ctx) error {
 	go func(user *models.User) {
 		partPlain := utils.NewPart().
 			SetBody("Hi " + user.Username + ",\n" +
-				"We noticed a new login to your UserStyles.world account.\n\n" +
-				"If that was you, you can safely ignore this e-mail.\n" +
-				"If that wasn't you, change your password:\n" +
+				"We noticed a new sign in to your UserStyles.world account.\n\n" +
+				"If that was you, you can safely ignore this email.\n" +
+				"If that wasn't you, please change your password:\n" +
 				"https://userstyles.world/account#password")
 		partHTML := utils.NewPart().
 			SetBody("<p>Hi " + user.Username + ",</p>\n" +
 				"<br>\n" +
-				"<p>We noticed a new login to your UserStyles.world account.</p>\n" +
+				"<p>We noticed a new sign in to your UserStyles.world account.</p>\n" +
 				"<br>\n" +
-				"<b>If that was you, you can safely ignore this e-mail.\n" +
+				"<b>If that was you, you can safely ignore this email.\n" +
 				"<br>\n" +
-				"<b>If that wasn't you, <a target=\"_blank\" clicktracking=\"off\" href=\"https://userstyles.world/account#password\">change your password</a>.\n").
+				"<b>If that wasn't you, <a target=\"_blank\" clicktracking=\"off\" href=\"https://userstyles.world/account#password\">please change your password</a>.\n").
 			SetContentType("text/html")
 
 		utils.NewEmail().
 			SetTo(user.Email).
-			SetSubject("New login").
+			SetSubject("New sign-in").
 			AddPart(*partPlain).
 			AddPart(*partHTML).
 			SendEmail(config.IMAPServer)
