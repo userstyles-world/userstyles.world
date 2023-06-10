785d96c5ef0303e8fd3ac1d19a4df7d56e41978d
vednoc <vednoc@protonmail.com>
2023-06-10 23:57:00 +0200

    chore(db): add migration for source code files

--- modules/database/init/migrate.go
+++ modules/database/init/migrate.go
@@ -1,6 +1,7 @@
 package init
 
 import (
+	"strconv"
 	"time"
 
 	"gorm.io/gorm"
@@ -19,12 +20,24 @@ func runMigration(db *gorm.DB) {
 	// Wrap in a transaction to allow rollbacks.
 	db.Transaction(func(tx *gorm.DB) error {
 		var s models.Style
-
 		if err := tx.Migrator().AddColumn(s, "ImportPrivate"); err != nil {
 			log.Database.Fatalf("Failed to add column import_private: %s\n", err)
 		}
 		if err := tx.Migrator().AddColumn(s, "MirrorPrivate"); err != nil {
 			log.Database.Fatalf("Failed to add column mirror_private: %s\n", err)
+		}
+
+		var styles []models.Style
+		err := tx.Debug().Select("id, code").Find(&styles).Error
+		if err != nil {
+			log.Database.Fatalf("Failed to find styles: %s\n", err)
+		}
+
+		for _, style := range styles {
+			err = models.SaveStyleCode(strconv.Itoa(int(style.ID)), style.Code)
+			if err != nil {
+				log.Warn.Fatalf("Failed to save %d: %s\n", style.ID, err)
+			}
 		}
 
 		return nil
