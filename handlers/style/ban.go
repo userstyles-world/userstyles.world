userstyles-world/userstyles.world

commit d8bc72a3addf99815556d4452afdb4ad97c3c7fb

refactor(email): use templates
Fixed bugs, added bugs, changed things. Some tested, some untested, some result in panic, vednoc pls fix.

handlers/style/ban.go
@@ -1,6 +1,7 @@
 package style
 
 import (
+	"bytes"
 	"strconv"
 	"strings"
 
@@ -44,43 +45,31 @@ func BanGet(c *fiber.Ctx) error {
 	})
 }
 
-func sendBanEmail(baseURL string, user *models.User, style *models.APIStyle, modLogID uint, reason string, message string) error {
+func sendBanEmail(c *fiber.Ctx, baseURL string, user *models.User, style *models.APIStyle, modLogID uint, reason string, message string) error {
 	modLogEntry := baseURL + "/modlog#id-" + strconv.Itoa(int(modLogID))
 
-	partPlain := utils.NewPart().
-		SetBody("Hi " + user.Username + ",\n" +
-			"We'd like to notice you about a recent action from our moderation team:\n\n" +
-			"Your style \"" + style.Name + "\" has been removed from our platform for the following reason:\n" +
-			reason + "\n\n" +
-			"Additional message from the moderator:\n" +
-			message + "\n\n" +
-			"This action is recorded in the modlog: " + modLogEntry + "\n\n" +
-			"If you'd like to come in touch with us, please email us at feedback@userstyles.world\n\n" +
-			"Regards,\n" + "The Moderation Team")
-	partHTML := utils.NewPart().
-		SetBody("<p>Hi " + user.Username + ",</p>\n" +
-			"<p>We'd like to notice you about a recent action from our moderation team:</p>\n" +
-			"<br>\n" +
-			"<p>Your style \"<b>" + style.Name + "</b>\" has been removed from our platform for the following reason:\n" +
-			reason + "</p>\n" +
-			"<br>\n" +
-			"<p>Additional message from the moderator:\n" +
-			message + "</p>\n" +
-			"<br>\n" +
-			"<p>This action is recorded in the " +
-			"<a target=\"_blank\" clicktracking=\"off\" href=\"" + modLogEntry + "\">Modlog</a>.</p>\n" +
-			"<br>\n" +
-			"<p>If you'd like to come in touch with us, " +
-			"please email us at <a href=\"mailto:feedback@userstyles.world\">feedback@userstyles.world</a>.</p>\n" +
-			"<br>\n" +
-			"<p>Regards,</p>\n" + "<p>The Moderation Team</p>").
-		SetContentType("text/html")
+	args := fiber.Map{
+		"Reason":  reason,
+		"Message": message,
+		"Link":    modLogEntry,
+	}
+
+	var bufText bytes.Buffer
+	var bufHTML bytes.Buffer
+	errText := c.App().Config().Views.Render(&bufText, "email/styleremoved.text", args)
+	errHTML := c.App().Config().Views.Render(&bufHTML, "email/styleremoved.html", args)
+	if errText != nil || errHTML != nil {
+		return c.Status(fiber.StatusInternalServerError).Render("err", fiber.Map{
+			"Title": "Internal server error",
+			"Error": "Failed to render email templates.",
+		})
+	}
 
 	err := utils.NewEmail().
 		SetTo(user.Email).
 		SetSubject("Your style has been removed").
-		AddPart(*partPlain).
-		AddPart(*partHTML).
+		AddPart(*utils.NewPart().SetBody(bufText.String())).
+		AddPart(*utils.NewPart().SetBody(bufHTML.String()).HTML()).
 		SendEmail(config.IMAPServer)
 	if err != nil {
 		return err
@@ -183,7 +172,7 @@ func BanPost(c *fiber.Ctx) error {
 		}
 
 		// Notify the author about style removal.
-		if err := sendBanEmail(baseURL, targetUser, style, modLogID, reason, message); err != nil {
+		if err := sendBanEmail(c, baseURL, targetUser, style, modLogID, reason, message); err != nil {
 			log.Warn.Printf("Failed to mail author for style %d: %s", style.ID, err.Error())
 		}
 	}(c.BaseURL(), s, logEntry.ID, logEntry.Reason, logEntry.Message)

handlers/style/promote.go
@@ -1,6 +1,7 @@
 package style
 
 import (
+	"bytes"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,7 +14,7 @@ import (
 	"userstyles.world/utils"
 )
 
-func sendPromotionEmail(userID uint, style *models.APIStyle, modName, baseURL string) {
+func sendPromotionEmail(c *fiber.Ctx, userID uint, style *models.APIStyle, modName, baseURL string) {
 	user, err := models.FindUserByID(strconv.Itoa(int(userID)))
 	if err != nil {
 		log.Warn.Printf("Couldn't find user %d: %s", userID, err.Error())
@@ -22,28 +23,25 @@ func sendPromotionEmail(userID uint, style *models.APIStyle, modName, baseURL st
 
 	modProfile := baseURL + "/user/" + modName
 
-	partPlain := utils.NewPart().
-		SetBody("Hi " + user.Username + ",\n" +
-			"We'd like to notice you about a recent action from our moderation team:\n\n" +
-			"Your style \"" + style.Name + "\" has been promoted to be featured on the homepage!\n" +
-			"It has been promoted by the moderator" + modName + ", checkout their profile: " + modProfile + ".\n\n" +
-			"Regards,\n" + "The Moderation Team")
-	partHTML := utils.NewPart().
-		SetBody("<p>Hi " + user.Username + ",</p>\n" +
-			"<p>We'd like to notice you about a recent action from our moderation team:</p>\n" +
-			"<br>\n" +
-			"<p>Your style \"" + style.Name + "\" has been promoted to be featured on the homepage!\n</p>\n" +
-			"<p>It has been promoted by the moderator " +
-			"<a target=\"_blank\" clicktracking=\"off\" href=\"" + modProfile + "\">" + modName + "</a>.</p>\n" +
-			"<br>\n" +
-			"<p>Regards,</p>\n" + "<p>The Moderation Team</p>").
-		SetContentType("text/html")
+	args := fiber.Map{
+		"ModName":    modName,
+		"ModProfile": modProfile,
+	}
+
+	var bufText bytes.Buffer
+	var bufHTML bytes.Buffer
+	errText := c.App().Config().Views.Render(&bufText, "email/stylepromoted.text", args)
+	errHTML := c.App().Config().Views.Render(&bufHTML, "email/stylepromoted.html", args)
+	if errText != nil || errHTML != nil {
+		log.Warn.Printf("Failed to render email template: %v%v\n", errText, errHTML)
+		return
+	}
 
 	err = utils.NewEmail().
 		SetTo(user.Email).
 		SetSubject("Your style is being featured").
-		AddPart(*partPlain).
-		AddPart(*partHTML).
+		AddPart(*utils.NewPart().SetBody(bufText.String())).
+		AddPart(*utils.NewPart().SetBody(bufHTML.String()).HTML()).
 		SendEmail(config.IMAPServer)
 	if err != nil {
 		log.Warn.Println("Failed to send email:", err.Error())
@@ -98,7 +96,7 @@ func Promote(c *fiber.Ctx) error {
 	// Ahem!!! We don't save the new value of Featured to the current style.
 	// So we have to reverse check it ;)
 	if !style.Featured {
-		go sendPromotionEmail(style.UserID, style, u.Username, c.BaseURL())
+		go sendPromotionEmail(c, style.UserID, style, u.Username, c.BaseURL())
 
 		// Create a notification.
 		notification := models.Notification{

handlers/user/ban.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"bytes"
 	"strconv"
 	"strings"
 
@@ -46,34 +47,30 @@ func Ban(c *fiber.Ctx) error {
 	})
 }
 
-func sendBanEmail(baseURL string, user *models.User, modLogID uint) error {
+func sendBanEmail(c *fiber.Ctx, baseURL string, user *models.User, modLogID uint, reason string) error {
 	modLogEntry := baseURL + "/modlog#id-" + strconv.Itoa(int(modLogID))
 
-	partPlain := utils.NewPart().
-		SetBody("Hi " + user.Username + ",\n" +
-			"We'd like to notice you about a recent action from our moderation team:\n\n" +
-			"You have been banned from our platform.\n" +
-			"You can check for more information about this action on the modlog: " + modLogEntry + "\n\n" +
-			"If you'd like to come in touch with us,please email us at feedback@userstyles.world\n\n" +
-			"Regards,\n" + "The Moderation Team")
-	partHTML := utils.NewPart().
-		SetBody("<p>Hi " + user.Username + ",</p>\n" +
-			"<p>We'd like to notice you about a recent action from our moderation team:</p>\n" +
-			"<br>\n" +
-			"<p>You have been banned from our platform.</p>\n" +
-			"<p>You can check for more information about this action on the " +
-			"<a target=\"_blank\" clicktracking=\"off\" href=\"" + modLogEntry + "\">Modlog</a>.</p>\n" +
-			"<p>If you'd like to come in touch with us, please email us at " +
-			"<a href=\"mailto:feedback@userstyles.world\">feedback@userstyles.world</a>.</p>\n" +
-			"<br>\n" +
-			"<p>Regards,</p>\n" + "<p>The Moderation Team</p>").
-		SetContentType("text/html")
+	args := fiber.Map{
+		"Reason": reason,
+		"Link":   modLogEntry,
+	}
+
+	var bufText bytes.Buffer
+	var bufHTML bytes.Buffer
+	errText := c.App().Config().Views.Render(&bufText, "email/userbanned.text", args)
+	errHTML := c.App().Config().Views.Render(&bufHTML, "email/userbanned.html", args)
+	if errText != nil || errHTML != nil {
+		return c.Status(fiber.StatusInternalServerError).Render("err", fiber.Map{
+			"Title": "Internal server error",
+			"Error": "Failed to render email templates.",
+		})
+	}
 
 	err := utils.NewEmail().
 		SetTo(user.Email).
 		SetSubject("You have been banned").
-		AddPart(*partPlain).
-		AddPart(*partHTML).
+		AddPart(*utils.NewPart().SetBody(bufText.String())).
+		AddPart(*utils.NewPart().SetBody(bufHTML.String()).HTML()).
 		SendEmail(config.IMAPServer)
 	if err != nil {
 		return err
@@ -149,12 +146,12 @@ func ConfirmBan(c *fiber.Ctx) error {
 		})
 	}
 
-	go func(baseURL string, user *models.User, modLogID uint) {
+	go func(baseURL string, user *models.User, modLogID uint, reason string) {
 		// Send a email about they've been banned.
-		if err := sendBanEmail(baseURL, targetUser, modLogID); err != nil {
+		if err := sendBanEmail(c, baseURL, targetUser, modLogID, reason); err != nil {
 			log.Warn.Printf("Failed to send an email to user %d: %s", user.ID, err.Error())
 		}
-	}(c.BaseURL(), targetUser, logEntry.ID)
+	}(c.BaseURL(), targetUser, logEntry.ID, logEntry.Reason)
 
 	return c.Redirect("/dashboard")
 }

handlers/user/login.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"bytes"
 	"errors"
 	"net/url"
 	"time"
@@ -123,24 +124,22 @@ func LoginPost(c *fiber.Ctx) error {
 	}
 
 	go func(user *models.User) {
-		partPlain := utils.NewPart().
-			SetBody("Hi " + user.Username + ",\n" +
-				"We noticed a new sign in to your UserStyles.world account.\n\n" +
-				"If that was you, you can safely ignore this email.\n" +
-				"If that wasn't you, please change your password:\n" +
-				"https://userstyles.world/account#password")
-		partHTML := utils.NewPart().
-			SetBody("<p>Hi " + user.Username + ",</p>\n" +
-				"<p>We noticed a new sign in to your UserStyles.world account.</p>\n" +
-				"<p>If that was you, you can safely ignore this email.<br>\n" +
-				"<b>If that wasn't you, please <a target=\"_blank\" clicktracking=\"off\" href=\"https://userstyles.world/account#password\">change your password</a>.</b></p>\n").
-			SetContentType("text/html")
+		args := fiber.Map{}
+
+		var bufText bytes.Buffer
+		var bufHTML bytes.Buffer
+		errText := c.App().Config().Views.Render(&bufText, "email/signin.text", args)
+		errHTML := c.App().Config().Views.Render(&bufHTML, "email/signin.html", args)
+		if errText != nil || errHTML != nil {
+			log.Warn.Printf("Failed to render email template: %v\n", err)
+			return
+		}
 
 		utils.NewEmail().
 			SetTo(user.Email).
 			SetSubject("New sign-in").
-			AddPart(*partPlain).
-			AddPart(*partHTML).
+			AddPart(*utils.NewPart().SetBody(bufText.String())).
+			AddPart(*utils.NewPart().SetBody(bufHTML.String()).HTML()).
 			SendEmail(config.IMAPServer)
 	}(user)
 

handlers/user/recover.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"bytes"
 	"errors"
 	"time"
 
@@ -78,27 +79,24 @@ func RecoverPost(c *fiber.Ctx) error {
 
 		link := config.BaseURL + "/reset/" + utils.EncryptText(jwtToken, utils.AEADCrypto, config.ScrambleConfig)
 
-		partPlain := utils.NewPart().
-			SetBody("Hi " + user.Username + ",\n" +
-				"We have received a request to reset the password for your UserStyles.world account.\n\n" +
-				"Follow the link bellow to reset your password. The link will expire in 4 hours.\n" +
-				link + "\n\n" +
-				"You can safely ignore this email if you didn't request to reset your password.")
-		partHTML := utils.NewPart().
-			SetBody("<p>Hi " + user.Username + ",</p>\n" +
-				"<p>We have received a request to reset the password for your UserStyles.world account.</p>\n" +
-				"<br>\n" +
-				"<p>Click the link bellow to reset your password. <b>The link will expire in 4 hours.</b><br>\n" +
-				"<a target=\"_blank\" clicktracking=\"off\" href=\"" + link + "\">Reset your password</a></p>\n" +
-				"<br>\n" +
-				"<p>You can safely ignore this email if you didn't request to reset your password.</p>").
-			SetContentType("text/html")
+		args := fiber.Map{
+			"Link": link,
+		}
+
+		var bufText bytes.Buffer
+		var bufHTML bytes.Buffer
+		errText := c.App().Config().Views.Render(&bufText, "email/passwordrecovery.text", args)
+		errHTML := c.App().Config().Views.Render(&bufHTML, "email/passwordrecovery.html", args)
+		if errText != nil || errHTML != nil {
+			log.Warn.Printf("Failed to render email template: %v\n", err)
+			return
+		}
 
 		utils.NewEmail().
 			SetTo(u.Email).
 			SetSubject("Reset your password").
-			AddPart(*partPlain).
-			AddPart(*partHTML).
+			AddPart(*utils.NewPart().SetBody(bufText.String())).
+			AddPart(*utils.NewPart().SetBody(bufHTML.String()).HTML()).
 			SendEmail(config.IMAPServer)
 	}(u)
 

handlers/user/reset.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"bytes"
 	"errors"
 
 	"github.com/go-playground/validator/v10"
@@ -124,29 +125,23 @@ func ResetPost(c *fiber.Ctx) error {
 	// Sends email that the password has been changed.
 	// But we do it in a separate routine, so we can render the view for the user.
 	go func(user *models.User) {
-		partPlain := utils.NewPart().
-			SetBody("Hi " + user.Username + ",\n" +
-				"We'd like to notice you about a recent action of your account:\n\n" +
-				"We've authorized a password change. Which was verified by email.\n" +
-				"If you don't recognize this account, please email us at feedback@userstyles.world.\n\n" +
-				"Regards,\n" + "The UserStyles.world team")
-		partHTML := utils.NewPart().
-			SetBody("<p>Hi " + user.Username + ",</p>\n" +
-				"<p>We'd like to notice you about a recent action of your account:</p>\n" +
-				"<br>\n" +
-				"<p>We've authorized a password change. Which was verified by email.</p>\n" +
-				"<br>\n" +
-				"<p>If you don't recognize this account, " +
-				"please email us at <a href=\"mailto:feedback@userstyles.world\">feedback@userstyles.world</a>.</p>\n" +
-				"<br>\n" +
-				"<p>Regards,</p>\n" + "<p>The UserStyles.world team</p>").
-			SetContentType("text/html")
+
+		args := fiber.Map{}
+
+		var bufText bytes.Buffer
+		var bufHTML bytes.Buffer
+		errText := c.App().Config().Views.Render(&bufText, "email/passwordreset.text", args)
+		errHTML := c.App().Config().Views.Render(&bufHTML, "email/passwordreset.html", args)
+		if errText != nil || errHTML != nil {
+			log.Warn.Printf("Failed to render email template: %v\n", err)
+			return
+		}
 
 		err := utils.NewEmail().
 			SetTo(user.Email).
 			SetSubject("Your password has been changed").
-			AddPart(*partPlain).
-			AddPart(*partHTML).
+			AddPart(*utils.NewPart().SetBody(bufText.String())).
+			AddPart(*utils.NewPart().SetBody(bufHTML.String()).HTML()).
 			SendEmail(config.IMAPServer)
 		if err != nil {
 			log.Warn.Println("Failed to send an email:", err.Error())

web/views/email/greeting.html.tmpl
@@ -1,0 +1,1 @@
+<p>Hi {{ .user.Username }},</p>

web/views/email/greeting.text.tmpl
@@ -1,0 +1,1 @@
+Hi {{ .user.Username }},

commit 9844649b3b56cff7774052cd8a192f56c3a5d529

fix(email): remove fiber.Ctx from async emails
This resolves panics introduced by me in d8bc72a3addf99815556d4452afdb4ad97c3c7fb.

handlers/style/ban.go
@@ -46,7 +46,7 @@ func BanGet(c *fiber.Ctx) error {
 	})
 }
 
-func sendBanEmail(c *fiber.Ctx, baseURL string, user *models.User, style *models.APIStyle, modLogID uint, reason string, message string) error {
+func sendBanEmail(baseURL string, user *models.User, style *models.APIStyle, modLogID uint, reason string, message string) error {
 	modLogEntry := baseURL + "/modlog#id-" + strconv.Itoa(int(modLogID))
 
 	args := fiber.Map{
@@ -58,10 +58,8 @@ func sendBanEmail(c *fiber.Ctx, baseURL string, user *models.User, style *models
 	var bufText, bufHTML bytes.Buffer
 	err := email.Render(&bufText, &bufHTML, "styleremoved", args)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).Render("err", fiber.Map{
-			"Title": "Internal server error",
-			"Error": "Failed to render email templates.",
-		})
+		log.Warn.Printf("Failed to render email template: %v\n", err)
+		return err
 	}
 
 	err = utils.NewEmail().
@@ -171,7 +169,7 @@ func BanPost(c *fiber.Ctx) error {
 		}
 
 		// Notify the author about style removal.
-		if err := sendBanEmail(c, baseURL, targetUser, style, modLogID, reason, message); err != nil {
+		if err := sendBanEmail(baseURL, targetUser, style, modLogID, reason, message); err != nil {
 			log.Warn.Printf("Failed to mail author for style %d: %s", style.ID, err.Error())
 		}
 	}(c.BaseURL(), s, logEntry.ID, logEntry.Reason, logEntry.Message)

handlers/style/promote.go
@@ -15,7 +15,7 @@ import (
 	"userstyles.world/utils"
 )
 
-func sendPromotionEmail(c *fiber.Ctx, userID uint, style *models.APIStyle, modName, baseURL string) {
+func sendPromotionEmail(userID uint, style *models.APIStyle, modName, baseURL string) {
 	user, err := models.FindUserByID(strconv.Itoa(int(userID)))
 	if err != nil {
 		log.Warn.Printf("Couldn't find user %d: %s", userID, err.Error())
@@ -95,7 +95,7 @@ func Promote(c *fiber.Ctx) error {
 	// Ahem!!! We don't save the new value of Featured to the current style.
 	// So we have to reverse check it ;)
 	if !style.Featured {
-		go sendPromotionEmail(c, style.UserID, style, u.Username, c.BaseURL())
+		go sendPromotionEmail(style.UserID, style, u.Username, c.BaseURL())
 
 		// Create a notification.
 		notification := models.Notification{

handlers/user/ban.go
@@ -48,7 +48,7 @@ func Ban(c *fiber.Ctx) error {
 	})
 }
 
-func sendBanEmail(c *fiber.Ctx, baseURL string, user *models.User, modLogID uint, reason string) error {
+func sendBanEmail(baseURL string, user *models.User, modLogID uint, reason string) error {
 	modLogEntry := baseURL + "/modlog#id-" + strconv.Itoa(int(modLogID))
 
 	args := fiber.Map{
@@ -59,10 +59,8 @@ func sendBanEmail(c *fiber.Ctx, baseURL string, user *models.User, modLogID uint
 	var bufText, bufHTML bytes.Buffer
 	err := email.Render(&bufText, &bufHTML, "userbanned", args)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).Render("err", fiber.Map{
-			"Title": "Internal server error",
-			"Error": "Failed to render email templates.",
-		})
+		log.Warn.Printf("Failed to render email template: %v\n", err)
+		return err
 	}
 
 	err = utils.NewEmail().
@@ -147,7 +145,7 @@ func ConfirmBan(c *fiber.Ctx) error {
 
 	go func(baseURL string, user *models.User, modLogID uint, reason string) {
 		// Send a email about they've been banned.
-		if err := sendBanEmail(c, baseURL, targetUser, modLogID, reason); err != nil {
+		if err := sendBanEmail(baseURL, targetUser, modLogID, reason); err != nil {
 			log.Warn.Printf("Failed to send an email to user %d: %s", user.ID, err.Error())
 		}
 	}(c.BaseURL(), targetUser, logEntry.ID, logEntry.Reason)