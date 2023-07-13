dd4cf55599688579580f0c59d2ffeb3caa677ab9
vednoc <vednoc@protonmail.com>
2023-07-13 18:57:24 +0200

    refactor(ui): update design for new review buttons

--- handlers/review/view.go
+++ handlers/review/view.go
@@ -11,19 +11,27 @@ func viewPage(c *fiber.Ctx) error {
 	u, _ := jwt.User(c)
 	c.Locals("User", u)
 
-	i, err := c.ParamsInt("r")
-	if err != nil || i < 1 {
+	sid, err := c.ParamsInt("s")
+	if err != nil || sid < 1 {
+		c.Locals("Title", "Invalid style ID")
+		return c.Status(fiber.StatusBadRequest).Render("err", fiber.Map{})
+	}
+
+	rid, err := c.ParamsInt("r")
+	if err != nil || rid < 1 {
 		c.Locals("Title", "Invalid review ID")
 		return c.Status(fiber.StatusBadRequest).Render("err", fiber.Map{})
 	}
-	c.Locals("Title", "Review "+c.Params("r"))
 
-	r, err := models.GetReview(i)
+	r, err := models.GetReview(rid)
 	if err != nil {
 		c.Locals("Title", "Review not found")
 		return c.Status(fiber.StatusNotFound).Render("err", fiber.Map{})
 	}
 	c.Locals("Review", r)
+
+	s, err := models.GetStyle(c.Params("s"))
+	c.Locals("Title", "Review for "+s.Name)
 
 	return c.Render("review/view", fiber.Map{})
 }
