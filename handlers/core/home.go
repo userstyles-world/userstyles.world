a3cf4ea95eb33edbe2df550b6bd748a9a95740e4
θ̫͖͑͋ε̧̺̊̄ó̪̱̒͝ς <42794343+a0eoc@users.noreply.github.com>
2023-08-04 17:55:47 +0500

    feat(homepage): show 16 random promoted styles

--- handlers/core/home.go
+++ handlers/core/home.go
@@ -1,12 +1,9 @@
 package core
 
 import (
-	"time"
-
 	"github.com/gofiber/fiber/v2"
 
 	"userstyles.world/handlers/jwt"
-	"userstyles.world/modules/cache"
 	"userstyles.world/modules/log"
 	"userstyles.world/modules/storage"
 )
@@ -32,8 +29,8 @@ func Home(c *fiber.Ctx) error {
 		}
 	*/
 
-Styles:
-	featured, found := cache.Store.Get("featuredStyles")
+	//Styles:
+	//featured, found := cache.Store.Get("featuredStyles")
 	updatedStyles, err := storage.FindStyleCardsPaginated(1, 4, "styles.updated_at DESC")
 	if err != nil {
 		//idk
@@ -42,20 +39,20 @@ Styles:
 	if err != nil {
 		//idk
 	}
-	if !found {
-		styles, err := storage.FindStyleCardsFeatured()
-		if err != nil {
-			log.Warn.Println("Couldn't get featured styles, due", err)
-			return c.Render("core/home", fiber.Map{
-				"Title":  "Home",
-				"User":   u,
-				"Styles": nil,
-			})
-		}
-		cache.Store.Set("featuredStyles", styles, 5*time.Minute)
-
-		goto Styles
+	//if !found {
+	featured, err := storage.FindStyleCardsFeatured()
+	if err != nil {
+		log.Warn.Println("Couldn't get featured styles, due", err)
+		return c.Render("core/home", fiber.Map{
+			"Title":  "Home",
+			"User":   u,
+			"Styles": nil,
+		})
 	}
+	//	cache.Store.Set("featuredStyles", styles, 5*time.Minute)
+
+	//	goto Styles
+	//}
 
 	return c.Render("core/home", fiber.Map{
 		"Title":         "Website themes and skins",
