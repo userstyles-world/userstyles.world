edfa9da54136bbe009e6fdbf307977aa63650bf9
vednoc <vednoc@protonmail.com>
2025-03-16 19:57:51 +0100

    refactor(migrator): tidy up second migration

--- modules/database/migrator/migrations.go
+++ modules/database/migrator/migrations.go
@@ -1,11 +1,10 @@
 package migrator
 
 import (
-	"fmt"
-
 	"gorm.io/gorm"
 
 	"userstyles.world/models"
+	"userstyles.world/modules/config"
 	"userstyles.world/modules/log"
 )
 
@@ -14,28 +13,33 @@ func m1(db *gorm.DB) error {
 }
 
 func m2(db *gorm.DB) error {
-	f := []string{"slug", "code_size", "code_checksum"}
-	for _, f := range []string{"Slug", "CodeSize", "CodeChecksum"} {
-		if err := db.Migrator().AddColumn(models.Style{}, f); err != nil {
-			return err
-		}
+	cx := []string{"slug", "code_size", "code_checksum"}
 
-		log.Database.Printf("Added '%s' column to 'styles' table.\n", f)
+	var s models.Style
+	for _, c := range cx {
+		if !db.Migrator().HasColumn(s, c) {
+			if err := db.Migrator().AddColumn(s, c); err != nil {
+				return err
+			}
+			log.Database.Printf("Added '%s' column to 'styles' table.\n", c)
+		}
 	}
 
 	var sx []models.Style
-	err := db.FindInBatches(&sx, 500, func(batch *gorm.DB, i int) error {
-		for _, s := range sx {
+	return db.FindInBatches(&sx, 500, func(tx *gorm.DB, size int) error {
+		for i, s := range sx {
 			s.Prepare()
-			if err := db.Model(s).Select(f).UpdateColumns(s).Error; err != nil {
+			if err := tx.Select(cx).UpdateColumns(s).Error; err != nil {
 				return err
 			}
 
-			fmt.Printf("%8d | %8d | %8s | %.40s\n", s.ID, s.CodeSize, s.CodeChecksum, s.Slug)
+			// Print everything in debug mode or every 100th message.
+			if config.App.Debug || i%100 == 0 {
+				const f = "id=%-8d cs=%-8d cc=%-8s s=%.40s\n"
+				log.Database.Printf(f, s.ID, s.CodeSize, s.CodeChecksum, s.Slug)
+			}
 		}
 
 		return nil
 	}).Error
-
-	return err
 }
